7cce823034433fef967c7b19500d1597308791f6
markomih <markomihajlovicfm@gmail.com>
2025-05-08 16:00:48 +0200

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='VolumetricSMPL',
-    version='1.0.3',
+    version='1.0.3',  # Bump version to trigger new upload
     packages=['VolumetricSMPL'],
     url='https://github.com/markomih/VolumetricSMPL',
     license='MIT',
@@ -13,6 +13,7 @@ setup(
     author_email='markomih@inf.ethz.ch',
     description='VolumetricSMPL body model.',
     long_description=long_description,
+    long_description_content_type='text/markdown',  # This tells PyPI it's Markdown
     python_requires='>=3.6.0',
     install_requires=[
         'numpy>=1.12.2',
